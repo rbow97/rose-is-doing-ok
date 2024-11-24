3dac6ac64590cb1a538db29ca485220cfdc1c57d
Rose Bowen <rosebowen1997@gmail.com>
2024-11-24 15:59:00 +0000

    scroll on mobile

--- app/hooks/useScrollSnap.ts
+++ app/hooks/useScrollSnap.ts
@@ -6,49 +6,27 @@ export function useScrollSnap(ref: RefObject<HTMLElement>) {
     if (!ref.current) return;
 
     const handleScroll = (event: WheelEvent) => {
+      if (window.innerWidth < LAYOUT.BREAKPOINTS.MOBILE) return;
+
       event.preventDefault();
-      const isMobile = window.innerWidth < LAYOUT.BREAKPOINTS.MOBILE;
       const container = ref.current;
       if (!container) return;
 
       const delta =
         Math.sign(event.deltaY) * Math.min(Math.abs(event.deltaY), 50);
-      const dimension = isMobile ? "offsetWidth" : "offsetHeight";
-      const scrollProp = isMobile ? "scrollLeft" : "scrollTop";
-      const currentScroll = container[scrollProp];
+      const currentScroll = container.scrollTop;
       const direction = delta > 0 ? 1 : -1;
 
       container.scrollTo({
-        [isMobile ? "left" : "top"]:
-          currentScroll + direction * container[dimension],
-        behavior: "smooth",
-      });
-    };
-
-    let touchStart = 0;
-    const handleTouchStart = (e: TouchEvent) => {
-      touchStart = e.changedTouches[0].screenX;
-    };
-
-    const handleTouchEnd = (e: TouchEvent) => {
-      if (!ref.current) return;
-      const touchEnd = e.changedTouches[0].screenX;
-      const direction = touchStart > touchEnd ? 1 : -1;
-
-      ref.current.scrollTo({
-        left: ref.current.scrollLeft + direction * ref.current.offsetWidth,
+        top: currentScroll + direction * container.offsetHeight,
         behavior: "smooth",
       });
     };
 
     window.addEventListener("wheel", handleScroll, { passive: false });
-    window.addEventListener("touchstart", handleTouchStart);
-    window.addEventListener("touchend", handleTouchEnd);
 
     return () => {
       window.removeEventListener("wheel", handleScroll);
-      window.removeEventListener("touchstart", handleTouchStart);
-      window.removeEventListener("touchend", handleTouchEnd);
     };
   }, [ref]);
 }
